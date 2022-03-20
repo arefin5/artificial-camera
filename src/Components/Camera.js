7c36b2fe6a98cec698709882ab60c47f1bb8df82
ghp_HCkHraWGiuXMv1JAMNiCeYoRdqgG1r3mc <arefintalukder5@gmail.com>
2022-03-20 15:06:58 +0600

    error-handle-done

--- src/Components/Camera.js
+++ src/Components/Camera.js
@@ -4,54 +4,51 @@ import { Container, Row, Col } from "react-bootstrap";
 import imageplaceholder from "../assets/image/imageplaceholder.png";
 import { FaCamera, FaFolder } from "react-icons/fa";
 import Webcam from "react-webcam";
-import {SweetAlert} from 'react-bootstrap-sweetalert';
+import SweetAlert from 'react-bootstrap-sweetalert';
+
 class Camera extends Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
-    this.cameraRef=React.createRef();  
-     this.state = {
-         capturedPhoto: imageplaceholder,
-         cameraError: false
-     }    
-}
-    onCapture = () =>{
-      console.log(this.cameraRef.current.getScreenshot());
-        let photoBase64 = this.cameraRef.current.getScreenshot();
-// console.log("photoBase64");
-        this.setState({
-            capturedPhoto: photoBase64
-        });
+    this.cameraRef = React.createRef();
+    this.state = {
+      capturedPhoto: imageplaceholder,
+      CameraError: false,
+    };
+  }
+  onCapture = () => {
+    // console.log(this.cameraRef.current.getScreenshot());
+    let photoBase64 = this.cameraRef.current.getScreenshot();
+    // console.log("photoBase64");
+    this.setState({
+      capturedPhoto: photoBase64,
+    });
+  };
+  // download photo
+  onSave = () => {
+    let photoBase64String = this.state.capturedPhoto;
+    const download = document.createElement("a");
+    download.href = photoBase64String;
+    download.download = "image.png";
+    download.click();
+  };
+  // handle camera error
+  handleCameraError = () => {
+    this.setState({
+      CameraError: true,
+    });
+  };
+  // handle erroralert
+  CameraErrorAlert = () => {
+    if (this.state.CameraError === true) {
+     return (
+      <SweetAlert danger title="Your Device Camera in not working " onConfirm={this.onCameraTryAgain}></SweetAlert>
+     )
     }
-    // download photo
-    onSave = () => {
-      let photoBase64String = this.state.capturedPhoto;  
-      const download=document.createElement("a");
-      download.href=photoBase64String;
-      download.download="image.png";
-      download.click();
-
-    }
-    // handle camera error
-    handleCameraError = () => {
-        this.setState({
-            cameraError: true
-        });
-    }
-    // handle erroralert
-    CameraErrorAlert=() =>{
-       
-        if(this.state.cameraError===true){
-   
-// 
-<SweetAlert warning title="Good job!" onConfirm={this.onCameraTryAgain} >
-</SweetAlert>
-
-        }
-    }
-    // reload page: 
-onCameraTryAgain=()=>{
-  window.location.href = "/";
-}
+  };
+  // reload page:
+  onCameraTryAgain = () => {
+    window.location.href = "/";
+  };
   render() {
     return (
       <Fragment>
@@ -60,13 +57,17 @@ onCameraTryAgain=()=>{
             <Col className="p-2" md={6} sm={12} lg={6}>
               {/* <img className="w-100" src={imageplaceholder} alt="imageplaceholder"  /> */}
               <Webcam
+                onUserMediaError={this.handleCameraError}
                 className="w-100"
                 audio={false}
                 screenshotFormat="image/jpeg"
                 ref={this.cameraRef}
-                onUserMediaError={this.handleCameraError}
+              
               />
-              <button onClick={this.onCapture} className="btn  btn-primary mt-3 btn-lg">
+              <button
+                onClick={this.onCapture}
+                className="btn  btn-primary mt-3 btn-lg"
+              >
                 <FaCamera />
                 Save
               </button>
@@ -77,17 +78,21 @@ onCameraTryAgain=()=>{
                 src={this.state.capturedPhoto}
                 alt="imageplaceholder"
               />
-              <button onClick={this.onSave} className="btn  btn-primary mt-3 btn-lg">
+              <button
+                onClick={this.onSave}
+                className="btn  btn-primary mt-3 btn-lg"
+              >
                 <FaFolder />
                 Save
               </button>
             </Col>
           </Row>
         </Container>
+        {this.CameraErrorAlert()}
       </Fragment>
     );
   }
 }
 
 export default Camera;
-//   optics zone  
+//   optics zone
